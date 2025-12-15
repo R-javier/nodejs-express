11baefc20324004388f5c36428764f193cd976fc
JLeizza-K <jleizza@kopiustech.com>
2025-12-15 14:10:49 +0000

    refactored index.js to use queries module

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
 import express from "express";
 import { Pool } from "pg";
-import 'dotenv/config'
+import "dotenv/config";
 import { parse } from "dotenv";
 import { queries } from "./queries";
-
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -20,7 +19,7 @@ app.use(express.json());
 
 app.get("/employees", async (req, res) => {
   try {
-    const data = await pool.query("SELECT * FROM empleados");
+    const data = await pool.query(queries.employees.getAll);
     res.json(data.rows);
   } catch (error) {
     console.log(error);
@@ -29,15 +28,7 @@ app.get("/employees", async (req, res) => {
 
 app.get("/employees/maxrole", async (req, res) => {
   try {
-    const maxrole = await pool.query(`SELECT 
-    r.id,
-    r.nombre,
-    COUNT(e.id) AS cantidad_empleados
-    FROM roles r
-    LEFT JOIN empleados e ON r.id = e.rol_id
-    GROUP BY r.id, r.nombre
-    ORDER BY cantidad_empleados DESC
-    LIMIT 1`);
+    const maxrole = await pool.query(queries.roles.getMaxRole);
 
     res.json(maxrole.rows);
   } catch (error) {
@@ -49,7 +40,7 @@ app.get("/employees/count/:dept", async (req, res) => {
   const deptId = req.params.dept;
   try {
     const deptCountEmployees = await pool.query(
-      "SELECT COUNT(*) FROM empleados WHERE departamento_id = $1",
+      queries.employees.countByDepartment,
       [deptId],
     );
     res.json(deptCountEmployees.rows);
@@ -63,7 +54,7 @@ app.get("/employees/:dept/:role", async (req, res) => {
   const roleId = req.params.role;
   try {
     const deptRoleEmployees = await pool.query(
-      "SELECT * FROM empleados WHERE departamento_id = $1 AND rol_id= $2",
+      queries.employees.getByDepartmentAndRole,
       [deptId, roleId],
     );
     res.json(deptRoleEmployees.rows);
@@ -75,10 +66,9 @@ app.get("/employees/:dept/:role", async (req, res) => {
 app.get("/employees/:dept", async (req, res) => {
   const deptId = req.params.dept;
   try {
-    const deptEmployees = await pool.query(
-      "SELECT * FROM empleados WHERE departamento_id = $1",
-      [deptId],
-    );
+    const deptEmployees = await pool.query(queries.employees.getByDepartment, [
+      deptId,
+    ]);
     res.json(deptEmployees.rows);
   } catch (error) {
     console.log(error);
@@ -89,31 +79,21 @@ app.put("/employees/:id/:role", async (req, res) => {
   const employeeId = req.params.id;
   const roleId = req.params.role;
   try {
-    const employee = await pool.query("SELECT * FROM empleados WHERE id = $1", [
-      employeeId,
+    const employee = await pool.query(queries.employees.getById, [employeeId]);
+    await pool.query(queries.employees.updateRole, [roleId, employeeId]);
+    const deptmEmployees = await pool.query(queries.employees.getByDepartment, [
+      employee.rows[0].departamento_id,
     ]);
-    await pool.query("UPDATE empleados SET rol_id = $1 WHERE id = $2", [
-      roleId,
-      employeeId,
-    ]);
-    const deptmEmployees = await pool.query(
-      "SELECT * FROM empleados WHERE departamento_id = $1",
-      [employee.rows[0].departamento_id],
-    );
     res.json(deptmEmployees.rows);
   } catch (error) {
     console.log(error);
   }
 });
 
-//VER antes de crear imagen
 app.delete("/employees/:id", async (req, res) => {
   const employeeId = req.params.id;
   try {
-    const employee = await pool.query(
-      "DELETE FROM empleados WHERE id = $1 RETURNING *",
-      [employeeId],
-    );
+    const employee = await pool.query(queries.employees.delete, [employeeId]);
 
     if (employee.rowCount === 0) {
       return res.status(404).json({ error: "Empleado no encontrado" });
